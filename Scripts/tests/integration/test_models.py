a82047d9d41e051b945fd261f47f90c4ab6411a2
Jens West <jens.west@traficom.fi>
2025-11-18 16:01:43 +0200

    Fix expected value in test for car mode share

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -54,7 +54,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.36127576237031267)
+            0.294199066252978)
         
         print("Model system test done")
 
