fbf12ab35f79fb163fb821a9f8e66b7d07714fe1
attesn <atte.supponen@outlook.com>
2025-12-15 15:18:43 +0200

    Use network walk distance in demand parameters

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -74,7 +74,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_leisure"],
-            0.5369451394696166)
+            0.5314767375987661)
 
     def _validate_impedances(self, impedances):
         self.assertIsNotNone(impedances)
