bae8534b098c84654cf4fbad191b0634fe23abb7
attesn <atte.supponen@outlook.com>
2025-12-30 13:54:54 +0200

    Convert to dest-mode model structure

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -77,7 +77,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_leisure"],
-            0.5976577777750404)
+            0.6336669507512368)
 
     def _validate_impedances(self, impedances):
         self.assertIsNotNone(impedances)
