50d938d25f61300dd908b2da051ac409b01ed0ff
attesn <atte.supponen@outlook.com>
2025-12-30 12:25:53 +0200

    Replace size factor distance intervals with attraction vars

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -77,7 +77,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_leisure"],
-            0.5975094970282102)
+            0.5976577777750404)
 
     def _validate_impedances(self, impedances):
         self.assertIsNotNone(impedances)
