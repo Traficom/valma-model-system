d8f963a46327f419c0ab5ea9aceb2e0c2590e143
Jens West <jens.west@traficom.fi>
2026-01-09 11:03:55 +0200

    Fix expected value in model integration test

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -54,7 +54,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_work"] + model.mode_share[0]["car_leisure"],
-            0.339610239478677)
+            0.3316256412414029)
         
         print("Model system test done")
 
