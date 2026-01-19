3d060fdd135ad9cfdfbfa4dfe5b3e7c026a0fc68
attesn <atte.supponen@outlook.com>
2026-01-19 15:54:12 +0200

    Calibrate mode choice constants Match mode shares to survey after assignment parameter changes

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -77,7 +77,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_leisure"],
-            0.6336669507512368)
+            0.636514822974174)
 
     def _validate_impedances(self, impedances):
         self.assertIsNotNone(impedances)
