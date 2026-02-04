853cbaa5c79f0d57a5f35c9fcdcc63f279a3ce1e
Jens West <jens.west@traficom.fi>
2026-02-04 15:59:14 +0200

    Fix calibration file paths

--- Scripts/valma_travel.py
+++ Scripts/valma_travel.py
@@ -37,6 +37,8 @@ def main(args):
     cost_data_file = Path(args.cost_data_file)
     result_data_folder = Path(args.result_data_folder, args.scenario_name)
     emme_project_path = Path(args.emme_project_file)
+    mode_dest_calibration_file = Path(args.mode_dest_calibration_file)
+    municipality_calibration_file = Path(args.municipality_calibration_file)
     log_extra = {
         "status": {
             "name": args.scenario_name,
@@ -101,8 +103,7 @@ def main(args):
     log.info("Initializing matrices and models...", extra=log_extra)
     model_args = (zone_data_file, cost_data_file,
                   base_matrices_path, result_data_folder, ass_model, args.submodel,
-                  args.mode_dest_calibration_file,
-                  args.municipality_calibration_file,
+                  mode_dest_calibration_file, municipality_calibration_file,
                   long_dist_matrices_path, freight_matrices_path)
     model = (AgentModelSystem(*model_args) if args.is_agent_model
              else ModelSystem(*model_args))
