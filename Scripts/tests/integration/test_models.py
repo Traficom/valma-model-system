c94dfeb24cc0b1009816f397002c7cc13678027c
attesn <atte.supponen@outlook.com>
2026-03-24 12:16:37 +0200

    Update mode-dest parameters for overnight and business

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -77,7 +77,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_drv"],
-            0.6217745420237623)
+            0.6218414948797577)
 
     def _validate_impedances(self, impedances):
         self.assertIsNotNone(impedances)
