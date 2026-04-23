c10dedf580ddce24d096d3deb3382419641fdd12
attesn <atte.supponen@outlook.com>
2026-04-23 15:19:14 +0300

    Update demand model tour rates Better handling of omitted tours to increase traffic volumes

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -57,7 +57,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_drv"],
-            0.3558165157911995)
+            0.35546688937487486)
         
         print("Model system test done")
 
@@ -77,7 +77,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_drv"],
-            0.6218414948797577)
+            0.6128165687377738)
 
     def _validate_impedances(self, impedances):
         self.assertIsNotNone(impedances)
