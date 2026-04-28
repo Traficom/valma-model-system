b74824d13294af9c7c40587ab814021509c71077
Jens West <jens.west@traficom.fi>
2026-04-28 10:10:53 +0300

    Fix expected value in model result assertion

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -77,7 +77,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_drv"],
-            0.6484626122525866)
+            0.638679192226269)
 
     def _validate_impedances(self, impedances):
         self.assertIsNotNone(impedances)
