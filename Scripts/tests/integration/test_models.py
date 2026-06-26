9af4f69b6b62d7993cd18e147e14b16819d9f0ea
attesn <atte.supponen@outlook.com>
2026-06-26 09:51:12 +0300

    Education size variables from workers to students Size with all education workers leads to too good accessibility in rural areas

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -57,7 +57,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_drv"],
-            0.3475946117416711)
+            0.3483426156339639)
         
         print("Model system test done")
 
