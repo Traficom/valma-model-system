26229e391d733a197d97f8749e6254d6117be7df
attesn <60136658+attesn@users.noreply.github.com>
2026-07-01 13:21:18 +0300

    Update travel demand model parameters (#422)
    * Add subarea dummies for transit and bike modes
    * Remove population from grocery size factors
    * Education size variables from workers to students
    * Add density variable to bike utility
    * Flip home-based work structure
    * Remove loop trips from visit and leisure tours
    * Estimate with transfer penalty impedance matrices
    * Rename overnight purpose

--- Scripts/tests/integration/test_models.py
+++ Scripts/tests/integration/test_models.py
@@ -57,7 +57,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_drv"],
-            0.3224965350851107)
+            0.3657308153681171)
         
         print("Model system test done")
 
@@ -77,7 +77,7 @@ class ModelTest(unittest.TestCase):
         # Check that model result does not change
         self.assertAlmostEquals(
             model.mode_share[0]["car_drv"],
-            0.6818506022562181)
+            0.682233110221325)
 
     def _validate_impedances(self, impedances):
         self.assertIsNotNone(impedances)
