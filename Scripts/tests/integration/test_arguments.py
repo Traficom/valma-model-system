8ce27a03ee1f729add5dcfeef87fa0d17ff3ba33
attesn <atte.supponen@traficom.fi>
2026-09-25 09:52:01 +0300

    Remove loop from argument calls

--- Scripts/tests/integration/test_arguments.py
+++ Scripts/tests/integration/test_arguments.py
@@ -1,7 +1,6 @@
 
 from pathlib import Path
 import numpy
-from travel_iteration import DEMAND_MATRIX_FOLDER
 
 TEST_DATA_PATH = Path(__file__).parent.parent / "test_data"
 RESULTS_PATH = TEST_DATA_PATH / "Results" / "test"
@@ -9,7 +8,7 @@ ZONEDATA_PATH = TEST_DATA_PATH / "Scenario_input_data" / "zonedata_test.gpkg"
 COSTDATA_PATH = TEST_DATA_PATH / "Scenario_input_data" / "costdata.json"
 MODE_DEST_CALIBRATION_FILE = TEST_DATA_PATH / "Scenario_input_data" / "mode_dest_calibration.json"
 MUNICIPALITY_CALIBRATION_FILE = TEST_DATA_PATH / "Scenario_input_data" / "municipality_calibration.txt"
-BASE_MATRICES_PATH = TEST_DATA_PATH / "Scenario_input_data" / DEMAND_MATRIX_FOLDER
+BASE_MATRICES_PATH = TEST_DATA_PATH / "Scenario_input_data" / "demand_matrices"
 INTERNAL_ZONES = [
     202, 1344, 1755, 2037, 2129, 2224, 2333, 2413, 2519, 2621, 2707, 2814, 2918,
     3000, 3003, 3203, 3302, 3416, 3639, 3705, 3800, 4013, 4102, 4202]
